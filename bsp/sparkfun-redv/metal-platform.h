eb24175ceabec37a7ad9d5429ef52c573c2747d7
Chao Yang <chao.yang@siteminder.com>
2022-08-31 21:03:39 +1200

    add missing package.json

--- bsp/sparkfun-redv/metal-platform.h
+++ bsp/sparkfun-redv/metal-platform.h
@@ -1,6 +1,6 @@
 /* Copyright 2019 SiFive, Inc */
 /* SPDX-License-Identifier: Apache-2.0 */
-/* ----------------------------------- */
+/* -------------redboard-v------------ */
 /* ----------------------------------- */
 
 #ifndef METAL_PLATFORM_H
